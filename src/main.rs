83a2453e731600981c1692750a894a4b60a58f6b
Oshi Paulinus <eminent2547@gmail.com>
2026-08-07 13:39:46 +0100

    add save to db and create account function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 #![allow(unused)]
 
-use std::println;
+use std::{io::{self, BufReader, BufWriter}, println};
 use serde::{Serialize, Deserialize};
+use std::fs::File;
 
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -15,14 +16,8 @@ struct Account {
 
 impl Account {
     fn new(name: &str, phone_no:&str) -> Self {
-        let mut account_no = String::new();
-
-        if phone_no.len() < 11 {
-            println!("Phone number has be 11 characters");
-        }else {
-            account_no = generate_account_number(phone_no);
-        }
-
+        
+        account_no = generate_account_number(phone_no);
         
         Self {
             account_name: String::from(name), 
@@ -34,7 +29,40 @@ impl Account {
 }
 
 fn main() {
+    println!("CLI BANKING SYSTEM");
     
+    loop {
+        println!("List of Operations:\n1. Create Account\n2. View Account\n3. Deposit Money\n4. Withdraw Money\n5. Transfer Money\n6. List Accounts\n7. Exit");
+
+        let input = user_input("Select an Option:");
+
+        match input.trim() {
+            "1" => {
+                let full_name = user_input("Enter your Full Name:");
+                let phone_number = user_input("Enter your Phone Number");
+
+                let phone_number = match phone_number.trim().parse::<i64>() {
+                    Ok(num) => num,
+                    Err(err) => {
+                        println!("Phone number should be only number digits");
+                        return;
+                    }
+                };
+
+                let phone_number = phone_number.to_string();
+                if phone_number.len() < 11 {
+                    println!("Phone number should be up to 11 digits")
+                }else {
+                    create_account(&full_name, &phone_number);
+                }
+            },
+
+
+            _ => {
+                println!("Invalid Input")
+            }
+        }
+    }
 }
 
 fn generate_account_number(phone_no:&str) -> String {
@@ -42,4 +70,83 @@ fn generate_account_number(phone_no:&str) -> String {
     let new_number = &phone_no[number..];
 
     String::from(new_number)
+}
+
+fn user_input(option:&str) -> String {
+
+    println!("{}", option);
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("No input found!");
+
+
+    input
+}
+
+fn load_data(path:&str) -> Vec<Account>{
+
+    let file = match File::open(path) {
+        Ok(file ) => file,
+        Err(err) => {
+            println!("File filed to open with the following error: {}", err);
+            return Vec::new();
+        }
+    };
+
+    let _reader = BufReader::new(file);
+
+    let db: Vec<Account> = match serde_json::from_reader(_reader) {
+        Ok(tasks) => tasks,
+        Err(err) => {
+            println!("Cant Deserialize the file with following err: {}", err);
+            return Vec::new();
+        }
+    };
+
+    db
+}
+
+
+fn save_to_db(path:&str, database: &Vec<Account>) -> bool {
+    let file = match File::create(path) {
+        Ok(file) => file,
+        Err(err) => {
+            println!("File not Created with the error: {}", err);
+            return false;
+        }
+    };
+
+    let _writer = BufWriter::new(file);
+
+    serde_json::to_writer_pretty(_writer, &database);
+
+    true
+}
+
+
+
+fn create_account(full_name:&str, phone_no:&str) {
+    let path = "database.json";
+    let db = load_data(path);
+    let mut acct_no = String::new();
+
+    for account in db.iter(){
+        if account.account_number == generate_account_number(phone_no){
+            println!("The Phone number has been used!");
+        }else {
+            acct_no = generate_account_number(phone_no)
+        }
+    }
+
+    let new_account = Account::new(full_name, phone_no);
+
+    db.push(new_account);
+
+    let saved = save_to_db(path, &db);
+
+    if saved{
+        println!("Account Created Successfully!");
+    }else {
+        println!("Account Not Creaeted")
+    }
+
 }
